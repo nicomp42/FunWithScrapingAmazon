1e97c72422963b519c36fc91c0c634a1338c72a3
nicomp42 <nicholdw@ucmail.uc.edu>
2022-11-27 14:00:43 -0500

    First Commit

---
+++ FunWithScrapingAmazon/mainPackage/main.py
@@ -0,0 +1,4 @@
+# main.py
+from scrapePackage.Scrape import *
+
+demo()
---
+++ FunWithScrapingAmazon/scrapePackage/Scrape.py
@@ -0,0 +1,10 @@
+#ScrapeCBOE.py
+
+# Need to install lxml
+# pip install lxml - use the Eclipse tool in the project properties
+# We don't need to import lxml
+
+from bs4 import BeautifulSoup
+import requests
+
+
